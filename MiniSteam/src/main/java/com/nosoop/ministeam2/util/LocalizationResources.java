24148d510713fdb8ffe12d7c8c47784d0c010b2a
nosoop <nosoop@users.noreply.github.com>
2014-05-26 02:37:12 -0700

    Added check for key existence in the LocalizationResources class.

--- MiniSteam/src/main/java/com/nosoop/ministeam2/util/LocalizationResources.java
+++ MiniSteam/src/main/java/com/nosoop/ministeam2/util/LocalizationResources.java
@@ -1,6 +1,8 @@
 package com.nosoop.ministeam2.util;
 
 import java.util.ResourceBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -8,6 +10,8 @@ import java.util.ResourceBundle;
  */
 public class LocalizationResources {
     private static ResourceBundle resource = null;
+    private static Logger logger = LoggerFactory.getLogger(
+            LocalizationResources.class.getSimpleName());
 
     private LocalizationResources() {
     }
@@ -16,6 +20,11 @@ public class LocalizationResources {
         if (resource == null) {
             resource = ResourceBundle.getBundle
                     ("com/nosoop/ministeam2/UIStrings");
+        }
+        
+        if (resource.containsKey(key)) {
+            logger.error("Could not find localization string for key {}.",
+                    key);
         }
         
         return resource.getString(key);
